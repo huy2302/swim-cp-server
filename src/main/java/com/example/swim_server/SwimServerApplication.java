67a7c79923fb0825ffbd130cdbe7d76c669c14db
huydevtr <huydevtr@gmail.com>
2026-04-07 08:41:25 +0700

    Init code

---
+++ src/main/java/com/example/swim_server/SwimServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.swim_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swim_server/SwimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swim_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
